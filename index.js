9b5d63f9002581c895aca8a17ce2d217fb38a22a
Iza Habjan <iza.habjan@gmail.com>
2022-06-02 12:55:08 +0200

    understanding require

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-require("./myscript.js");
-console.log(require.cache);
+const counterObject = require("./myscript.js");
+
+console.log(counterObject.getCounter()); // 0
+counterObject.incrementCounter();
+console.log(counterObject.getCounter()); // 1
+
+const newCounterObject = require("./myscript.js"); // not executed for the second time
+console.log(newCounterObject.getCounter()); // 1 (value that is currently in the require cache object)
--- myscript.js
+++ myscript.js
@@ -1,1 +1,10 @@
-module.exports = "hi there";
+let counter = 0;
+
+module.exports = {
+  incrementCounter() {
+    counter = counter + 1;
+  },
+  getCounter() {
+    return counter;
+  },
+};
